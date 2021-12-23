0accc3f86b14161bde1dfcb5a79db403b044cc6d
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-12-23 21:51:21 +0000

    more refactoring

--- src/app/_services/auth-guard.service.ts
+++ src/app/_services/auth-guard.service.ts
@@ -1,31 +1,44 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanActivateChild {
-  
-    constructor(private jwtHelper: JwtHelperService, private router: Router) { }
+
+  constructor(private router: Router, private _authenticationService: AuthenticationService) { }
+
+  ngOnInit(): void {
+    this._getAuthStatus();
+  }
+
+  private _getAuthStatus(): void {
+    this._authenticationService.checkAuthStatus();
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const token = localStorage.getItem('jwt');
-
-    if (token && !this.jwtHelper.isTokenExpired(token)) {
-      // console.log(this.jwtHelper.decodeToken(token));
+    if (this._authenticationService.isAuthorised) {
       return true;
+    } else {
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+      return false;
     }
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
-    return false;
   }
 
   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const token = localStorage.getItem('jwt');
+    //   const token = localStorage.getItem('jwt');
 
-    if (token && !this.jwtHelper.isTokenExpired(token)) {
-      // console.log(this.jwtHelper.decodeToken(token));
+    //   if (token && !this.jwtHelper.isTokenExpired(token)) {
+    //     // console.log(this.jwtHelper.decodeToken(token));
+    //     return true;
+    //   }
+    //   this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    //   return false;
+    // }
+    if (this._authenticationService.isAuthorised) {
       return true;
+    } else {
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+      return false;
     }
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
-    return false;
   }
 }
--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ReadTokenService } from '../_auth/read-token.service';
 import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
@@ -10,26 +9,15 @@ import { AuthenticationService } from '../_services/authentication.service';
 })
 export class NavMenuComponent implements OnInit {
   isNavbarCollapsed = true;
-  // need to read this from token service...
-  authenticatedUser: any; // UserViewModel;
 
   constructor(readonly _service: AuthenticationService) { }
 
   ngOnInit(): void {
     this._getAuthStatus();
-    // this._serv.GetAuthenticatedUserDetails();
   }
 
   private _getAuthStatus(): void {
     this._service.checkAuthStatus();
-  }
-
-  private _getAuthenticatedUser(): void {
-  //   if (this.isLoggedIn) {
-  //     this.authenticatedUser = this._service.GetAuthenticatedUserDetails();
-  //   } else {
-  //     this.authenticatedUser = null;
-  //   }
   }
 }
 

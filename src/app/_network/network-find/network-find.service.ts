38869b73939914cdc8ebaf25ff8fd767b85b00b6
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-01-20 22:55:32 +0000

    implementation code Network find

--- src/app/_network/network-find/network-find.service.ts
+++ src/app/_network/network-find/network-find.service.ts
@@ -1,9 +1,56 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, finalize, Observable } from 'rxjs';
+import { INetworkUser } from '../i-network-user.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkFindService {
 
-  constructor() { }
+  private readonly _isError$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private readonly _isLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private readonly _searchResults$: BehaviorSubject<INetworkUser[] | null> = new BehaviorSubject<INetworkUser[] | null>(null);
+
+  constructor(private readonly _httpClient: HttpClient) { }
+
+  public get isError(): Observable<boolean> {
+    return this._isError$.asObservable();
+  }
+
+  public get isLoading(): Observable<boolean> {
+    return this._isLoading$.asObservable();
+  }
+
+  public get getSearchResults(): Observable<INetworkUser[] | null> {
+    return this._searchResults$.asObservable();
+  }
+
+  public getData(searchCriterion: string): void {
+
+    this._isLoading$.next(true);
+
+    if (this._searchResults$) this.resetFeed();
+
+    const options = searchCriterion ?
+      { params: new HttpParams().set('searchCriterion', searchCriterion) } : {};
+
+    this._httpClient.get<INetworkUser[]>('api/Network/SearchNetwork', options)
+      .pipe(finalize(() => this._isLoading$.next(false)))
+      .subscribe({
+        next: (response) => {
+          this._searchResults$.next(response);
+        },
+        error: (e) => { this._handleError(e); }
+      })
+  }
+
+  public resetFeed(): void {
+    this._searchResults$.next(null);
+  }
+
+  private _handleError(error: any) { // no need to send error to the component...
+    //console.log(error);
+    this._isError$.next(true);
+  }
 }
--- src/app/_network/network-suggestion/network-suggestion.service.ts
+++ src/app/_network/network-suggestion/network-suggestion.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, finalize, Observable } from 'rxjs';
 import { INetworkUser } from '../i-network-user.dto';

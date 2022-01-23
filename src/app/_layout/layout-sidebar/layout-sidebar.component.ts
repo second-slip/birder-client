2bd956368d6de2fe05cf7236494f13399204ac81
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-01-23 10:10:29 +0000

    Navbar fixed to top

--- src/app/_layout/layout-sidebar/layout-sidebar.component.ts
+++ src/app/_layout/layout-sidebar/layout-sidebar.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-layout-sidebar',
   templateUrl: './layout-sidebar.component.html',
-  styleUrls: ['./layout-sidebar.component.scss']
+  styleUrls: ['./layout-sidebar.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LayoutSidebarComponent { }

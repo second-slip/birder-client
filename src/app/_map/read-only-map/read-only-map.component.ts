7625d433b542a21f8a858fd123844e3d4dc5414f
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-02-03 19:32:18 +0000

    readonly map implementation

--- src/app/_map/read-only-map/read-only-map.component.ts
+++ src/app/_map/read-only-map/read-only-map.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { IObservationPosition } from '../i-observation-position.dto';
 
 @Component({
   selector: 'app-read-only-map',
   templateUrl: './read-only-map.component.html',
-  styleUrls: ['./read-only-map.component.scss']
+  styleUrls: ['./read-only-map.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ReadOnlyMapComponent implements OnInit {
+  @Input() position: IObservationPosition;
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow;
+
+  public errorObject = null;
+
+  public locationMarker: any;
+  public options: google.maps.MapOptions = {
+    mapTypeId: 'terrain', zoom: 8,
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this._addMarker(this.position.latitude, this.position.longitude);
   }
 
+  public openInfoWindow(marker: MapMarker): void {
+    this.infoWindow.open(marker);
+  }
+
+  private _addMarker(latitude: number, longitude: number): void {
+    try {
+      this.locationMarker = ({
+        position: {
+          lat: latitude,
+          lng: longitude
+        },
+        options: { animation: google.maps.Animation.BOUNCE },
+      })
+    } catch (error) {
+      // ToDo: implementation
+      //this.errorObject = error;
+    }
+  }
 }

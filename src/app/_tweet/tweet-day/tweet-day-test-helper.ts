0d784977f192c3da7a76911b565735e2145cf3ff
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-06-22 22:49:36 +0100

    Test TweetDay service

---
+++ src/app/_tweet/tweet-day/tweet-day-test-helper.ts
@@ -0,0 +1,48 @@
+import { IBirdSummary } from "src/app/_bird/i-bird-summary.dto";
+import { ITweet } from "../i-tweet.dto";
+
+export const fakeIBirdSummary: IBirdSummary = {
+    birdId: 1113,
+    species: 'Vanellus vanellus',
+    englishName: 'Lapwing',
+    populationSize: '100,000 - 500,000 Pairs',
+    btoStatusInBritain: 'Migrant/Resident Breeder, Passage/Winter Visitor',
+    thumbnailUrl: null,
+    conservationStatus: '',
+    conservationListColourCode: '',
+    birderStatus: 'Common'
+}
+
+export const fakeITweet: ITweet = 
+{
+    tweetDayId: 1,
+    songUrl: 'https://www.xeno-canto.org/sounds/uploaded/BPSDQEOJWG/XC448690--Tofsvipa-varningsl%C3%A4te-Eneb%C3%A5gsudden%2C%20Brevik%20Vg-%282017-05-01%2012.05%29-LS140667.mp3',
+    displayDay: '2021-02-18T00:00:00Z',
+    creationDate: '2020-11-27T22:30:26.5866667Z',
+    lastUpdateDate: '2021-01-29T14:32:24.91Z',
+    bird: fakeIBirdSummary // {} as IBirdSummary
+};
+
+
+// important to check that the object is valid JSON!
+// use https://jsonlint.com/ to validate the object
+// it causes all sorts of issues if the object is not valid!!!!!
+export const fakeTweetResponse: any =
+{
+    "tweetDayId": 1,
+    "songUrl": "https://www.xeno-canto.org/sounds/uploaded/BPSDQEOJWG/XC448690--Tofsvipa-varningsl%C3%A4te-Eneb%C3%A5gsudden%2C%20Brevik%20Vg-%282017-05-01%2012.05%29-LS140667.mp3",
+    "displayDay": "2021-02-18T00:00:00Z",
+    "creationDate": "2020-11-27T22:30:26.5866667Z",
+    "lastUpdateDate": "2021-01-29T14:32:24.91Z",
+    "bird": {
+        "birdId": 1113,
+        "species": "Vanellus vanellus",
+        "englishName": "Lapwing",
+        "populationSize": "100,000 - 500,000 Pairs",
+        "btoStatusInBritain": "Migrant/Resident Breeder, Passage/Winter Visitor",
+        "thumbnailUrl": null,
+        "conservationStatus": "",
+        "conservationListColourCode": "",
+        "birderStatus": "Common"
+    }
+}
--- src/app/_tweet/tweet-day/tweet-day.service.spec.ts
+++ src/app/_tweet/tweet-day/tweet-day.service.spec.ts
@@ -1,16 +1,96 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ITweet } from '../i-tweet.dto';
+import { skip } from 'rxjs/operators';
 
 import { TweetDayService } from './tweet-day.service';
+import { fakeITweet, fakeTweetResponse } from './test-helper';
+
+const _apiUrl = 'api/Tweets';
 
 describe('TweetDayService', () => {
+  let controller: HttpTestingController;
   let service: TweetDayService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TweetDayService],
+    });
     service = TestBed.inject(TweetDayService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    controller.verify();
   });
+
+
+  it('makes an http call', () => {
+    // Arrange
+    let actualTweet: ITweet | null | undefined; // undefined initial state to check if Observable emits
+    let actualErrorState: boolean | undefined;
+    let finalLoadingState: boolean | undefined;
+
+    // Act
+    service.getData(); // call http request method
+
+    // We expect that the Observable emits an array that equals to the one from the API response:
+    service.getTweet.subscribe((ratesObservable) => {
+      actualTweet = ratesObservable
+    });
+
+    service.isError
+      .subscribe((error) => {
+        actualErrorState = error;
+      });
+
+    service.isLoading.pipe(skip(1)) // skip first default 'true' value emitted...
+      .subscribe((loading) => {
+        finalLoadingState = loading;
+      });
+
+    const request = controller.expectOne(_apiUrl); // _apiUrl);
+    // Answer the request so the Observable emits a value.
+    request.flush(fakeTweetResponse); // also paste the response object in with {}
+
+    // Assert
+    expect(actualTweet).toEqual(fakeITweet);
+    expect(actualErrorState).toBeFalse();
+    expect(finalLoadingState).toBeFalse();
+  });
+
+  it('#getData should use GET to retrieve data', () => {
+    service.getData();
+    const testRequest = controller.expectOne(_apiUrl);
+    expect(testRequest.request.method).toEqual('GET');
+  });
+
+  it('passes through errors', () => {
+    // Arrange
+    const status = 500;
+    const statusText = 'Internal Server Error';
+    const errorEvent = new ErrorEvent('API error');
+    let actualErrorState: boolean | undefined;
+    let finalLoadingState: boolean | undefined;;
+
+    // Act & Assert
+    service.getData(); // call http request method
+
+    service.isError.pipe(skip(1)) // skip first, default 'false' value emitted...
+      .subscribe((error) => {
+        actualErrorState = error;
+      });
+
+    service.isLoading.pipe(skip(1)) // skip first, default 'true' value emitted...
+      .subscribe((loading) => {
+        finalLoadingState = loading;
+      });
+
+    controller.expectOne(_apiUrl).error(errorEvent, { status, statusText });
+
+    expect(actualErrorState).toBeTrue();
+    expect(finalLoadingState).toBeFalse();
+  });
+
 });

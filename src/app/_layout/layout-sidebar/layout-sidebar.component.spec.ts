5e730cfa58d2a05f07b433724bbbacf96fabe477
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-06-26 13:20:05 +0100

    code tidying

--- src/app/_layout/layout-sidebar/layout-sidebar.component.spec.ts
+++ src/app/_layout/layout-sidebar/layout-sidebar.component.spec.ts
@@ -21,7 +21,7 @@ describe('LayoutSidebarComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });

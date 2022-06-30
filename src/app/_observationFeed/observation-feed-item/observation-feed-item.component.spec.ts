051549ff7fd25b5372e8265ac2ec5848d7740ec7
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-06-30 14:12:45 +0100

    unit test component

--- src/app/_observationFeed/observation-feed-item/observation-feed-item.component.spec.ts
+++ src/app/_observationFeed/observation-feed-item/observation-feed-item.component.spec.ts
@@ -1,25 +1,78 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthenticationService } from 'src/app/_auth/authentication.service';
+import { of } from 'rxjs';
 
-// import { ObservationFeedItemComponent } from './observation-feed-item.component';
+import { ObservationFeedItemComponent } from './observation-feed-item.component';
+import { IObservationFeed } from '../i-observation-feed.dto';
+import { IAuthUser } from '../../_auth/i-auth-user.dto';
+import { expectText } from 'src/app/testing/element.spec-helper';
 
-// describe('ObservationFeedItemComponent', () => {
-//   let component: ObservationFeedItemComponent;
-//   let fixture: ComponentFixture<ObservationFeedItemComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ ObservationFeedItemComponent ]
-//     })
-//     .compileComponents();
-//   });
+describe('ObservationFeedItemComponent', () => {
+    let component: ObservationFeedItemComponent;
+    let fixture: ComponentFixture<ObservationFeedItemComponent>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ObservationFeedItemComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    let fakeAuthService: AuthenticationService;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    const authUser: IAuthUser = {
+        userName: 'test',
+        avatar: 'testAvatar',
+        defaultLocationLatitude: 0.99,
+        defaultLocationLongitude: 0.88
+    }
+
+    const fakeIObservationFeedSingleItem: IObservationFeed =
+    {
+        observationId: 10099,
+        quantity: 1,
+        observationDateTime: "2021-11-28T17:02:13.527Z",
+        birdId: 1002,
+        species: "Stercorarius parasiticus",
+        englishName: "Arctic Skua",
+        thumbnailUrl: "https://farm66.staticflickr.com/65535/52183437764_25d56664db_q.jpg",
+        latitude: 53.44244782183897,
+        longitude: -2.2773529647976964,
+        formattedAddress: "581A Wilbraham Rd, Chorlton-cum-Hardy, Manchester M21 9AF, UK",
+        shortAddress: "Manchester, United Kingdom",
+        username: "monkey",
+        notesCount: 0,
+        creationDate: "2021-11-28T17:02:45.1213198Z",
+        lastUpdateDate: "2021-11-28T17:02:45.1213198Z"
+    }
+
+    fakeAuthService = jasmine.createSpyObj<AuthenticationService>(
+        'AuthenticationService',
+        {
+            checkAuthStatus: undefined,
+            logout: undefined
+        },
+        {
+            isAuthorisedObservable: of(false),
+            isAuthorised: false,
+            getAuthUser: of(authUser)
+        }
+    );
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ObservationFeedItemComponent],
+            providers: [{ provide: AuthenticationService, useValue: fakeAuthService }]
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ObservationFeedItemComponent);
+        component = fixture.componentInstance;
+        component.observation = fakeIObservationFeedSingleItem; // {} as IObservationFeed;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('renders the species name from the Input() object', () => {
+        expectText(fixture, 'species-name', String(fakeIObservationFeedSingleItem.species));
+    });
+});

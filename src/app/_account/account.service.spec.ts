9db72e807963097282833b13062f5bc5b5ce690f
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-09-05 00:11:25 +0100

    testing acount service

--- src/app/_account/account.service.spec.ts
+++ src/app/_account/account.service.spec.ts
@@ -1,14 +1,85 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { AccountService } from './account.service';
+import { IUserEmail } from './i-user-email.dto';
+import { IUsername } from './i-username.dto';
+
+const username = 'test-username';
+const email = 'test@email.net';
+const model = <IUserEmail>{ email: email };
+const usernameModel = <IUsername>{ username: username };
 
 describe('AccountService', () => {
   let service: AccountService;
+  let controller: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(AccountService);
+    controller = TestBed.inject(HttpTestingController);
   });
+
+  afterEach(() => {
+    controller.verify();
+  });
+
+  it('checks if the username is taken', () => {
+    let result: boolean | undefined;
+    service.isUsernameTaken(username).subscribe((otherResult) => {
+      result = otherResult;
+    });
+
+    const request = controller.expectOne({
+      method: 'POST',
+      url: 'api/account/check-username',
+    });
+    expect(request.request.body).toEqual({ model: { username: username } });
+    request.flush({ usernameTaken: true });
+
+    expect(result).toBe(true);
+  });
+
+  it('checks if the email is taken', () => {
+    let result: boolean | undefined;
+    service.isEmailTaken(email).subscribe((otherResult) => {
+      result = otherResult;
+    });
+
+    const request = controller.expectOne({
+      method: 'POST',
+      url: 'api/account/check-email',
+    });
+    expect(request.request.body).toEqual({ model: { email: email } });
+    request.flush({ emailTaken: true });
+
+    expect(result).toBe(true);
+  });
+
+
+  it('checks the requestPasswordReset model is posted', () => {
+    let result: boolean | undefined;
+    service.requestPasswordReset(model).subscribe((otherResult) => {
+      result = otherResult;
+    });
+
+    const request = controller.expectOne({
+      method: 'POST',
+      url: 'api/account/request-password-reset',
+    });
+    expect(request.request.body).toEqual({ email: email }); //: { email: email } });
+    request.flush({ success: true });
+
+    expect(result).toBe(true);
+  });
+
+
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({});
+  //   service = TestBed.inject(AccountService);
+  // });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
--- src/app/_account/account.service.ts
+++ src/app/_account/account.service.ts
@@ -18,33 +18,33 @@ export class AccountService {
   constructor(private readonly _http: HttpClient) { }
 
   public register(model: IAccountRegistration): Observable<IAccountRegistration> {
-    return this._http.post<IAccountRegistration>('api/Account/Register', model, httpOptions);
+    return this._http.post<IAccountRegistration>('api/account/register', model, httpOptions);
   }
 
-  public requestPasswordReset(model: IUserEmail): Observable<Boolean> {
-    return this._http.post<{ sucess: boolean }>('api/Account/request-password-reset', model, httpOptions)
-      .pipe(map((result) => result.sucess));
+  public requestPasswordReset(model: IUserEmail): Observable<boolean> {
+    return this._http.post<{ success: boolean }>('api/account/request-password-reset', model, httpOptions)
+      .pipe(map((result) => result.success));
   }
 
   public resendEmailConfirmation(model: IUserEmail): Observable<IUserEmail> {
-    return this._http.post<IUserEmail>('api/Account/ResendEmailConfirmation', model, httpOptions);
+    return this._http.post<IUserEmail>('api/account/resend-email-confirmation', model, httpOptions);
   }
 
   public resetPassword(model: IResetPassword): Observable<IResetPassword> {
-    return this._http.post<IResetPassword>('api/Account/ResetPassword', model, httpOptions);
+    return this._http.post<IResetPassword>('api/account/reset-password', model, httpOptions);
   }
 
   public isUsernameTaken(username: string): Observable<boolean> {
     const model = <IUsername>{ username: username };
 
-    return this._http.post<{ usernameTaken: boolean }>('api/Account/check-username', { model }, httpOptions) //are content type headers needed?
+    return this._http.post<{ usernameTaken: boolean }>('api/account/check-username', { model }, httpOptions)
       .pipe(map((result) => result.usernameTaken));
   }
 
   public isEmailTaken(email: string): Observable<boolean> {
     const model = <IUserEmail>{ email: email };
 
-    return this._http.post<{ emailTaken: boolean }>('api/Account/check-email', { model }, httpOptions) //are content type headers needed?
+    return this._http.post<{ emailTaken: boolean }>('api/account/check-email', { model }, httpOptions)
       .pipe(map((result) => result.emailTaken));
   }
 }

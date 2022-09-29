188cd4b66e646340b7b5fd7a8311b53eb4b9c2d5
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-09-29 12:39:25 +0100

    update unit tests

--- src/app/_bird/flickr/flickr.component.spec.ts
+++ src/app/_bird/flickr/flickr.component.spec.ts
@@ -1,89 +1,164 @@
-// import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { of, throwError } from 'rxjs';
-// import { expectText } from 'src/app/testing/element.spec-helper';
-// import { photoUrlsArray, species } from 'src/app/testing/flickr-recordings-api-tests-helper';
-// import { FlickrComponent } from './flickr.component';
-// import { FlickrService } from './flickr.service';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { expectText } from 'src/app/testing/element.spec-helper';
+import { photoUrlsArray } from 'src/app/testing/flickr-recordings-api-tests-helper';
+import { FlickrComponent } from './flickr.component';
+import { FlickrService } from './flickr.service';
 
-// describe('FlickrComponent', () => {
-//   let component: FlickrComponent;
-//   let fixture: ComponentFixture<FlickrComponent>;
+describe('FlickrComponent', () => {
+  let component: FlickrComponent;
+  let fixture: ComponentFixture<FlickrComponent>;
 
-//   let fakeFlickrService: jasmine.SpyObj<FlickrService>;
+  let fakeFlickrService: jasmine.SpyObj<FlickrService>;
 
-//   const setup = async (
-//     fakeFlickrReturnValues?: jasmine.SpyObjMethodNames<FlickrService>) => {
+  const setup = async (
+    fakePropertyValues?: jasmine.SpyObjPropertyNames<FlickrService>) => {
 
-//     fakeFlickrService = jasmine.createSpyObj<FlickrService>(
-//       'FlickrService',
-//       {
-//         getSearchResults: undefined,
-//         ...fakeFlickrReturnValues
-//       }
-//     );
+    fakeFlickrService = jasmine.createSpyObj<FlickrService>(
+      'FlickrService',
+      {
+        getData: undefined
+      },
+      {
+        images: of(photoUrlsArray),
+        isError: of(false),
+        ...fakePropertyValues
+      }
+    );
 
-//     await TestBed.configureTestingModule({
-//       declarations: [
-//         FlickrComponent
-//       ],
-//       providers: [
-//         { provide: FlickrService, useValue: fakeFlickrService }]
-//     }).compileComponents();
+    await TestBed.configureTestingModule({
+      declarations: [
+        FlickrComponent
+      ]
+    })
+      .overrideComponent(FlickrComponent,
+        {
+          set: {
+            providers: [{ provide: FlickrService, useValue: fakeFlickrService }]
+          }
+        })
+      .compileComponents();
 
-//     fixture = TestBed.createComponent(FlickrComponent);
-//     component = fixture.componentInstance;
-//     component.species = species;
-//     // fixture.detectChanges();
-//   };
+    fixture = TestBed.createComponent(FlickrComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  };
 
-//   it('should be created', fakeAsync(async () => {
-//     await setup(
-//       {
-//         getSearchResults: of(photoUrlsArray)
-//       }
-//     );
-//     expect(component).toBeTruthy();
-//   }));
+  it('should be created', fakeAsync(async () => {
+    await setup(
+      {
+        images: of(null),
+        isError: of(false),
+      }
+    );
+
+    expect(component).toBeTruthy();
+    const { debugElement } = fixture;
+    const loading = debugElement.query(By.css('app-loading'));
+    expect(loading).toBeTruthy();
+  }));
+
+  describe('when images are fetched', () => {
+    it('calls data fetch', fakeAsync(async () => {
+      await setup();
+      expect(fakeFlickrService.getData).toHaveBeenCalledTimes(1);
+    }));
+
+    it('shows the images', fakeAsync(async () => {
+      await setup();
+      const carousel = fixture.nativeElement as HTMLElement;
+      expect(carousel.querySelector('[data-testid="images"]')?.textContent).toBeDefined();
+    }));
+
+    it('does not show error section', fakeAsync(async () => {
+      await setup();
+      const w = fixture.nativeElement as HTMLElement;
+      expect(w.querySelector('[data-testid="error"]')?.textContent).toBeUndefined();
+    }));
+
+    it('does not show the loading section', fakeAsync(async () => {
+      await setup();
+
+      const { debugElement } = fixture;
+      const loading = debugElement.query(By.css('app-loading'));
+      expect(loading).toBeNull();
+    }));
+  });
 
 
-//   it('should render the photos', fakeAsync(async () => {
-//     await setup(
-//       {
-//         getSearchResults: of(photoUrlsArray)
-//       }
-//     );
+  describe('when fetched zero images', () => {
+    it('calls data fetch', fakeAsync(async () => {
+      await setup({
+        images: of([])
+      });
+      expect(fakeFlickrService.getData).toHaveBeenCalledTimes(1);
+    }));
 
-//     const carousel = fixture.nativeElement as HTMLElement;
-//     expect(carousel.querySelector('[data-testid="carousel"]')?.textContent).toBeUndefined();
-//     const error = fixture.nativeElement as HTMLElement;
-//     expect(error.querySelector('[data-testid="error"]')?.textContent).toBeUndefined();
+    it('shows no images section', fakeAsync(async () => {
+      await setup({
+        images: of([])
+      });
+      const noRecords = fixture.nativeElement as HTMLElement;
+      expect(noRecords.querySelector('[data-testid="no-images"]')?.textContent).toBeDefined();
+    }));
 
-//     fixture.detectChanges();
+    it('does not show error section', fakeAsync(async () => {
+      await setup({
+        images: of([])
+      });
+      const error = fixture.nativeElement as HTMLElement;
+      expect(error.querySelector('[data-testid="error"]')?.textContent).toBeUndefined();
+    }));
 
-//     expect(fakeFlickrService.getSearchResults).toHaveBeenCalledWith(species, undefined, undefined);
-//     expect(carousel.querySelector('[data-testid="carousel"]')?.textContent).toBeDefined();
-//     expect(error.querySelector('[data-testid="error"]')?.textContent).toBeUndefined();
-//   }));
+    it('does not show loading section', fakeAsync(async () => {
+      await setup({
+        images: of([])
+      });
+      const { debugElement } = fixture;
+      const loading = debugElement.query(By.css('app-loading'));
+      expect(loading).toBeNull();
+    }));
+  });
 
+  describe('on error', () => {
+    it('calls data fetch', fakeAsync(async () => {
+      await setup({
+        images: of(null),
+        isError: of(true)
+      });
+      expect(fakeFlickrService.getData).toHaveBeenCalledTimes(1);
+    }));
 
-//   it('should react to errors', fakeAsync(async () => {
-//     await setup(
-//       {
-//         getSearchResults: throwError(() => new Error('test'))
-//       }
-//     );
+    it('does not show no images section', fakeAsync(async () => {
+      await setup({
+        images: of(null),
+        isError: of(true)
+      });
+      const records = fixture.nativeElement as HTMLElement;
+      expect(records.querySelector('[data-testid="no-images"]')?.textContent).toBeUndefined();
+      const noRecords = fixture.nativeElement as HTMLElement;
+      expect(noRecords.querySelector('[data-testid="no-images"]')?.textContent).toBeUndefined();
+    }));
 
-//     const carousel = fixture.nativeElement as HTMLElement;
-//     expect(carousel.querySelector('[data-testid="carousel"]')?.textContent).toBeUndefined();
+    it('shows error section', fakeAsync(async () => {
+      await setup({
+        images: of(null),
+        isError: of(true)
+      });
+      expectText(fixture, 'error', 'Whoops! An error occurred. Please try again. ');
+      const error = fixture.nativeElement as HTMLElement;
+      expect(error.querySelector('[data-testid="error"]')?.textContent).toBeDefined();
+    }));
 
-//     const error = fixture.nativeElement as HTMLElement;
-//     expect(error.querySelector('[data-testid="error"]')?.textContent).toBeUndefined();
-
-//     fixture.detectChanges();
-
-//     expect(fakeFlickrService.getSearchResults).toHaveBeenCalledWith(species, undefined, undefined);
-//     expect(carousel.querySelector('[data-testid="carousel"]')?.textContent).toBeUndefined();
-//     expectText(fixture, 'error', 'Whoops! An error occurred. Please try again. ');
-//   }));
-// });
+    it('does not show loading section', fakeAsync(async () => {
+      await setup({
+        images: of(null),
+        isError: of(true)
+      });
+      const { debugElement } = fixture;
+      const loading = debugElement.query(By.css('app-loading'));
+      expect(loading).toBeNull();
+    }));
+  });
+});

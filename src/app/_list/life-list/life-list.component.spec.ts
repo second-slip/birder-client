60cd6926d8d1d09dc970866ac6690e1c61e5c545
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-10-15 15:37:16 +0100

    Testing

--- src/app/_list/life-list/life-list.component.spec.ts
+++ src/app/_list/life-list/life-list.component.spec.ts
@@ -2,6 +2,8 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
+import { expectText, findComponent, findEl } from 'src/app/testing/element.spec-helper';
+import { lifeListModel } from 'src/app/testing/list-test-helpers';
 import { ObservationCountService } from 'src/app/_analysis/observation-count/observation-count.service';
 import { LifeListComponent } from './life-list.component';
 import { LifeListService } from './life-list.service';
@@ -23,14 +25,12 @@ describe('LifeListComponent', () => {
     });
 
   const setup = async (
-    fakeMethodValues?: jasmine.SpyObjMethodNames<LifeListService>,
     fakePropertyValues?: jasmine.SpyObjPropertyNames<LifeListService>) => {
 
     fakeListService = jasmine.createSpyObj<LifeListService>(
       'LifeListService',
       {
-        getData: undefined,
-        ...fakeMethodValues
+        getData: undefined
       },
       {
         lifeList: of(null),
@@ -58,14 +58,126 @@ describe('LifeListComponent', () => {
 
 
   it('should be created and show the loading placeloader', fakeAsync(async () => {
-    await setup({}, {});
+    await setup({});
 
-    expect(component).toBeTruthy();
-    const { debugElement } = fixture;
-    const loading = debugElement.query(By.css('app-loading'));
-    expect(loading).toBeTruthy();
+    expect(findComponent(fixture, 'app-loading')).toBeTruthy();;
   }));
 
+  describe('when data fetch returns observations', () => {
+
+    it('calls data fetch', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(false),
+          lifeList: of(lifeListModel)
+        });
+
+      expect(fakeListService.getData).toHaveBeenCalledTimes(1);
+    }));
+
+    it('shows the list', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(false),
+          lifeList: of(lifeListModel)
+        });
+
+      expect(findEl(fixture, 'life-list-table')).toBeTruthy();
+    }));
+
+    it('does not show error section', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(false),
+          lifeList: of(lifeListModel)
+        });
+
+      const error = fixture.nativeElement as HTMLElement;
+      expect(error.querySelector('[data-testid="error"]')?.textContent).toBeUndefined();
+    }));
+
+    it('does not show loading section', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(false),
+          lifeList: of(lifeListModel)
+        });
+
+      const { debugElement } = fixture;
+      const loading = debugElement.query(By.css('app-loading'));
+      expect(loading).toBeNull();
+    }));
+
+    it('does not show no-items section when >0 items', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(false),
+          lifeList: of(lifeListModel)
+        });
+
+      const error = fixture.nativeElement as HTMLElement;
+      expect(error.querySelector('[data-testid="no-items"]')?.textContent).toBeUndefined();
+    }));
+
+    it('does show no-items section when =0 items', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(false),
+          lifeList: of([])
+        });
+
+      expect(findEl(fixture, 'no-items')).toBeTruthy();
+    }));
+  });
 
 
+  describe('when error fetching data', () => {
+
+    it('shows error content', fakeAsync(async () => {
+      await setup(
+        {
+
+          isError: of(true),
+          lifeList: of(null)
+        });
+
+      expect(findEl(fixture, 'error')).toBeTruthy();
+      expectText(fixture, 'error', 'Whoops! There was an error retrieving the data.Try Again');
+    }));
+
+    it('tries data fetch again on user button click', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(true),
+          lifeList: of(null)
+        });
+
+      fixture.debugElement.query(By.css('.btn-try-again')).triggerEventHandler('click', null);
+
+      expect(fakeListService.getData).toHaveBeenCalled();
+    }));
+
+    it('hides list content', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(true),
+          lifeList: of(null)
+        });
+
+      const error = fixture.nativeElement as HTMLElement;
+      expect(error.querySelector('[data-testid="life-list-table"]')?.textContent).toBeUndefined();
+    }));
+
+    it('does not show loading section', fakeAsync(async () => {
+      await setup(
+        {
+          isError: of(true),
+          lifeList: of(null)
+        });
+
+      const { debugElement } = fixture;
+      const loading = debugElement.query(By.css('app-loading'));
+      expect(loading).toBeNull();
+    }));
+  });
 });

dfede36f005302b1320f31e33d5dd6dd3bb16baa
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-02-23 13:10:17 +0000

    obfuscate tokens

---
+++ src/set-env.ts
@@ -0,0 +1,37 @@
+import { writeFile } from 'fs';
+
+// import {name, version} from '../package.json';
+
+const targetPath = './src/environments/environment.prod.ts';
+
+// const envConfigFile = `export const environment = {
+//    production: true,
+//    firebase: {
+//         apiKey: '${process.env.FIREBASE_API_KEY}'
+//     },
+//     name: '${name}',
+//     version: '${version}'
+// };
+// `;
+
+// const envConfigFile = `export const environment = {
+//     production: true,
+//     ...tokens.mapKey, //mapKey: 'AIzaSyD4IghqI4x7Sld9KP3sP6FtbN7wCPGySmY',
+//     apiUrl: 'https://birderweb.com',
+//     ...tokens.photoKey //: '03338e2c98b507aaf9fbd0f8f045581b'
+//   };
+//   `;
+
+const envConfigFile = `export const environment = {
+    production: true,
+    mapKey: '${process.env.MAPKEY}',
+    apiUrl: 'https://birderweb.com',
+    photoKey: '${process.env.PHOTOKEY}'
+  };
+  `;
+
+writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+    if (err) {
+        return console.log(err);
+    }
+});

a6e80e2257c1c52d3ac35bd427c2b01f5d31db10
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-02-25 13:14:19 +0000

    Code tidying

--- src/app/_validators/birdsList.validator.ts
+++ src/app/_validators/birdsList.validator.ts
@@ -1,10 +1,10 @@
 import { AbstractControl, ValidatorFn } from "@angular/forms"
 
 export function BirdsListValidator(): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any } | null => {
-      if (typeof control.value === 'string') {
-        return { 'notBirdListObject': { value: control.value } }
-      }
-      return null  /* valid option selected */
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    if (typeof control.value === 'string') {
+      return { 'notBirdListObject': { value: control.value } }
     }
+    return null  /* valid option selected */
   }
+}
--- src/app/_validators/dateTime.validator.ts
+++ src/app/_validators/dateTime.validator.ts
@@ -1,8 +1,8 @@
 import { AbstractControl, ValidatorFn } from "@angular/forms";
 
 export function DateValid(nameRe: RegExp): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: any} | null => {
-      const forbidden = nameRe.test(control.value);
-      return forbidden ? {'invalidDate': {value: control.value}} : null;
-    };
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? { 'invalidDate': { value: control.value } } : null;
+  };
 }

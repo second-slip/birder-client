0a8b4e60df17f2e7ec8981d776bcd797b90e24b2
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-03-29 13:19:52 +0100

    cleaning up

--- src/app/_observation-note/observation-note-type.ts
+++ src/app/_observation-note/observation-note-type.ts
@@ -5,11 +5,4 @@ export enum ObservationNoteType {
     Appearance = 'Appearance',
     Behaviour = 'Behaviour',
     Vocalisation = 'Vocalisation'
-
-    // General = 0,
-    // Habitat = 1,
-    // Weather = 2,
-    // Appearance = 3,
-    // Behaviour = 4,
-    // Vocalisation = 5
 }
--- src/app/testing/flickr-recordings-api-tests-helper.ts
+++ src/app/testing/flickr-recordings-api-tests-helper.ts
@@ -8,79 +8,79 @@ const flickrPhotoSearch = `${baseUrl}search&per_page=20&text=`;
 export const flickrUrl = `${flickrPhotoSearch}${encodeURIComponent(species)}&page=${1}`
 
 export const photoUrlsArray = Array<{ url: string }>(
-    { url: 'https://farm66.staticflickr.com/65535/52370492495_9143d838ec_z.jpg' },
-    { url: 'https://farm66.staticflickr.com/65535/52370455555_e7d81245c1_z.jpg' },
-    { url: 'https://farm66.staticflickr.com/65535/52365430181_de10631953_z.jpg' });
+  { url: 'https://farm66.staticflickr.com/65535/52370492495_<<example>>_z.jpg' },
+  { url: 'https://farm66.staticflickr.com/65535/52370455555_<<example>>_z.jpg' },
+  { url: 'https://farm66.staticflickr.com/65535/52365430181_<<example>>_z.jpg' });
 
 export const flickrResponse =
 {
-    "photos": {
-        "page": 1,
-        "pages": 126,
-        "perpage": 20,
-        "total": 2503,
-        "photo": [
-            {
-                "id": "52370492495",
-                "owner": "12240837@N08",
-                "secret": "9143d838ec",
-                "server": "65535",
-                "farm": 66,
-                "title": "Arctic Skua (stercorarius parasiticus)",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "52370455555",
-                "owner": "12240837@N08",
-                "secret": "e7d81245c1",
-                "server": "65535",
-                "farm": 66,
-                "title": "Skua sp",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "52365430181",
-                "owner": "12639178@N07",
-                "secret": "de10631953",
-                "server": "65535",
-                "farm": 66,
-                "title": "Schmarotzeraubmöwe",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            }
-        ]
-    }
+  "photos": {
+    "page": 1,
+    "pages": 126,
+    "perpage": 20,
+    "total": 2503,
+    "photo": [
+      {
+        "id": "52370492495",
+        "owner": "12240837@N08",
+        "secret": "<<example>>",
+        "server": "65535",
+        "farm": 66,
+        "title": "Arctic Skua (stercorarius parasiticus)",
+        "ispublic": 1,
+        "isfriend": 0,
+        "isfamily": 0
+      },
+      {
+        "id": "52370455555",
+        "owner": "12240837@N08",
+        "secret": "<<example>>",
+        "server": "65535",
+        "farm": 66,
+        "title": "Skua sp",
+        "ispublic": 1,
+        "isfriend": 0,
+        "isfamily": 0
+      },
+      {
+        "id": "52365430181",
+        "owner": "12639178@N07",
+        "secret": "<<example>>",
+        "server": "65535",
+        "farm": 66,
+        "title": "Schmarotzeraubmöwe",
+        "ispublic": 1,
+        "isfriend": 0,
+        "isfamily": 0
+      }
+    ]
+  }
 }
 
 export const recordingsResponse =
-[
-  {
-    "id": 0,
-    "url": "//xeno-canto.org/sounds/uploaded/POUECYRDLL/XC691581-arctic skua 0613.mp3"
-  },
-  {
-    "id": 1,
-    "url": "//xeno-canto.org/sounds/uploaded/POUECYRDLL/XC674548-pair arctic skua alarm calling 270521 0050.mp3"
-  },
-  {
-    "id": 2,
-    "url": "//xeno-canto.org/sounds/uploaded/SDPCHKOHRH/XC432780-2018-08-03 Husey - Borgafjordur 180803 Labbe parasite divers cris.mp3"
-  },
-  {
-    "id": 3,
-    "url": "//xeno-canto.org/sounds/uploaded/TLPLNAINFU/XC428823-20180727_006_TYVJO_CALL_RISVIK.mp3"
-  },
-  {
-    "id": 4,
-    "url": "//xeno-canto.org/sounds/uploaded/TLPLNAINFU/XC318713-160527-001831tyvjo_brholm.mp3"
-  },
-  {
-    "id": 5,
-    "url": "//xeno-canto.org/sounds/uploaded/CDTGHVBGZP/XC203457-Parasitic Jaeger_chase2014-6-28-6.mp3"
-  }
-]
+  [
+    {
+      "id": 0,
+      "url": "//xeno-canto.org/sounds/uploaded/POUECYRDLL/XC691581-arctic skua 0613.mp3"
+    },
+    {
+      "id": 1,
+      "url": "//xeno-canto.org/sounds/uploaded/POUECYRDLL/XC674548-pair arctic skua alarm calling 270521 0050.mp3"
+    },
+    {
+      "id": 2,
+      "url": "//xeno-canto.org/sounds/uploaded/SDPCHKOHRH/XC432780-2018-08-03 Husey - Borgafjordur 180803 Labbe parasite divers cris.mp3"
+    },
+    {
+      "id": 3,
+      "url": "//xeno-canto.org/sounds/uploaded/TLPLNAINFU/XC428823-20180727_006_TYVJO_CALL_RISVIK.mp3"
+    },
+    {
+      "id": 4,
+      "url": "//xeno-canto.org/sounds/uploaded/TLPLNAINFU/XC318713-160527-001831tyvjo_brholm.mp3"
+    },
+    {
+      "id": 5,
+      "url": "//xeno-canto.org/sounds/uploaded/CDTGHVBGZP/XC203457-Parasitic Jaeger_chase2014-6-28-6.mp3"
+    }
+  ]

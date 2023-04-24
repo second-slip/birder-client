5566a6c990a8c56bda3b9de34a29115e553f5145
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-04-24 16:17:59 +0100

    work on json server implementation

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
-    // res.header('X-Hello', 'World123')
-    // next()
+
     if (req.method === 'POST' && req.originalUrl === '/authentication/login') { // /authToken') {
         return res.jsonp({
             "authenticationToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkFuZHJldyIsIkltYWdlVXJsIjoiaHR0cHM6Ly9pbWcuaWNvbnM4LmNvbS9jb2xvci85Ni8wMDAwMDAvdXNlci5wbmciLCJMYXQiOiI1My40NDI1OTM0IiwiTG5nIjoiLTIuMjc2OTA1MiIsIkZsaWNrcktleSI6IjAzMzM4ZTJjOThiNTA3YWFmOWZiZDBmOGYwNDU1ODFiIiwianRpIjoiZjYzOTdiMjUtODdhNC00NTI4LTk1YTctMjMzY2FiMTI4Zjg5IiwiZXhwIjoxNzA5MzE3NDA3LCJpc3MiOiJodHRwczovL2JpcmRlcndlYi5jb20iLCJhdWQiOiJodHRwczovL2JpcmRlcndlYi5jb20ifQ.XEZ-zzPC-nJjC4JqFRuBjBaa0iwYbmwpx4ecVIHorgk",
@@ -9,7 +8,7 @@ module.exports = (req, res, next) => {
     }
 
     // Observation Analysis api
-    if (req.method === 'GET' && req.originalUrl === '/ObservationAnalysis') {
+    if (req.method === 'GET' && req.originalUrl === '/observationanalysis') {
         return res.jsonp(
             {
                 totalObservationsCount: Math.floor(Math.random() * 100) + 1,
@@ -19,8 +18,58 @@ module.exports = (req, res, next) => {
 
     // observation api
 
+    if (req.method === 'GET' && req.originalUrl === '/observation') {
 
-    if (req.method === 'POST' && req.originalUrl === '/createObservation') {
+        return res.jsonp(
+            {
+                observationId: 10094,
+                quantity: 1,
+                observationDateTime: new Date(),
+                creationDate: new Date(),
+                lastUpdateDate: new Date(),
+                birdId: 1005,
+                bird: {
+                    birdId: 1005,
+                    species: "Puffinus mauretanicus",
+                    englishName: "Balearic Shearwater",
+                    populationSize: "1,000 - 10,000 Birds",
+                    btoStatusInBritain: "Passage Visitor",
+                    thumbnailUrl: null,
+                    conservationStatus: "Red",
+                    conservationListColourCode: "Red",
+                    birderStatus: 0
+                },
+                "position": {
+                    observationPositionId: 78,
+                    latitude: 53.44244782183897,
+                    longitude: -2.2773529647976964,
+                    formattedAddress: "581A Wilbraham Rd, Chorlton-cum-Hardy, Manchester M21 9AF, UK",
+                    shortAddress: "Manchester, United Kingdom"
+                },
+                "user": {
+                    userName: "monkey",
+                    avatar: "https://img.icons8.com/color/96/000000/user.png",
+                    defaultLocationLatitude: 53.44244782183897,
+                    defaultLocationLongitude: -2.2773529647976964
+                },
+                "notes": [
+                    {
+                        id: 24,
+                        noteType: "General",
+                        note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    },
+                    {
+                        id: 25,
+                        noteType: "General",
+                        note: "yu"
+                    }
+                ]
+            }
+        )
+    }
+
+
+    if (req.method === 'POST' && req.originalUrl === '/createobservation') {
         return res.jsonp({
             observationId: 10094, // an object with this id exists in 'observations'
             quantity: req.body.quantity,
@@ -40,13 +89,13 @@ module.exports = (req, res, next) => {
         })
     }
 
-    if (req.method === 'DELETE' && req.originalUrl === '/deleteObservation') {
+    if (req.method === 'DELETE' && req.originalUrl === '/deleteobservation') {
         return res.jsonp({
             id: 1
         })
     }
 
-    if (req.method === 'PUT' && req.originalUrl === '/updateObservation') {
+    if (req.method === 'PUT' && req.originalUrl === '/updateobservation') {
         return res.jsonp({
             observationId: req.body.observationId, // an object with this id 10090 exists in 'observations'
             quantity: req.body.quantity,
@@ -221,6 +270,27 @@ module.exports = (req, res, next) => {
         }
 
         return res.jsonp(data);
+    }
+
+    // user profile
+    if (req.method === 'GET' && req.originalUrl === '/userprofile') {
+
+        return res.jsonp(
+            {
+                user: {
+                    userName: "monkey",
+                    avatar: "https://img.icons8.com/color/96/000000/user.png",
+                    isFollowing: false,
+                    isOwnProfile: true
+                },
+                registrationDate: "2021-06-14T23:26:54.1974072Z",
+                observationCount: {
+                    totalObservationsCount: Math.floor(Math.random() * 100) + 1,
+                    uniqueSpeciesCount: Math.floor(Math.random() * 55) + 1,
+                },
+                followersCount: Math.floor(Math.random() * 50) + 1,
+                followingCount: Math.floor(Math.random() * 60) + 1,
+            });
     }
 
     // Generate random data...

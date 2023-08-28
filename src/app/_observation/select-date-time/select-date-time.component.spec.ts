5d540e444f7339851e5b619474bb52b8d14128e4
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-08-28 11:01:41 +0100

    Integrate new date time picker component

--- src/app/_observation/select-date-time/select-date-time.component.spec.ts
+++ src/app/_observation/select-date-time/select-date-time.component.spec.ts
@@ -1,16 +1,22 @@
-import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { SelectDateTimeComponent } from './select-date-time.component';
 import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { click, expectText, findEl } from 'src/app/testing/element.spec-helper';
+
+const requiredFields = [
+  'date',
+  'time'
+];
 
 describe('SelectDateTimeComponent', () => {
   let component: SelectDateTimeComponent;
   let fixture: ComponentFixture<SelectDateTimeComponent>;
 
-  const setup = async () => {
+  const setup = async (fakeInputDate?: Date | null) => {
 
     await TestBed.configureTestingModule({
-      imports: [FormsModule, NgbModule],
+      imports: [FormsModule, NgbModule, NgbModule],
       declarations: [SelectDateTimeComponent]
     })
       .compileComponents();
@@ -19,12 +25,13 @@ describe('SelectDateTimeComponent', () => {
     component = fixture.componentInstance;
 
     component.dateForm = new FormGroup({
-      observationDateTime: new FormControl(new Date(), Validators.compose([
+      observationDateTime: new FormControl(fakeInputDate, Validators.compose([
         Validators.required
       ]))
     });
 
     //spyOn(component, 'reload');
+    spyOn(component, 'updateFormValue');
 
     fixture.detectChanges();
   };
@@ -32,32 +39,106 @@ describe('SelectDateTimeComponent', () => {
   describe('when component is loaded', () => {
 
     it('should create', fakeAsync(async () => {
-      await setup();
+      await setup(new Date());
       expect(component).toBeTruthy();
     }));
 
-    // it('should call the data fetch method', fakeAsync(async () => {
-    //   await setup();
-    //   expect(fakeSelectSpeciesService.getData).toHaveBeenCalledTimes(1);
-    // }));
 
-    // it('should show the loading symbol', fakeAsync(async () => {
-    //   await setup();
-    //   expect(findEl(fixture, 'loading')).toBeDefined();
-    // }));
+    describe('when form is valid', () => {
+
+      it('fields should be marked as required', fakeAsync(async () => {
+        await setup(new Date());
+
+        requiredFields.forEach((testId) => {
+          const el = findEl(fixture, testId);
+
+          expect(el.attributes['aria-required']).toBe(
+            'true',
+            `${testId} must be marked as aria-required`,
+          );
+        });
+      }));
+
+      it('should not show error message', fakeAsync(async () => {
+        await setup(new Date());
+
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('[data-testid="form-invalid-error-message"]')?.textContent).toBeUndefined();
+      }));
+    });
+
+    describe('when form is not valid', () => {
+
+      it('should show error message', fakeAsync(async () => {
+        await setup(null);
+
+        expectText(fixture, 'form-invalid-date-error-message', 'Pick a date using the control.');
+      }));
+    });
+
   });
 
+  // describe('when form is not valid', () => {
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [SelectDateTimeComponent]
-//     });
-//     fixture = TestBed.createComponent(SelectDateTimeComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  //   // it('should show error message', fakeAsync(async () => {
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
- });
+  //   //   // spyOn(component, 'updateFormValue');
+
+  //   //   await setup(new Date());
+
+  //   //   // expect(component).toBeTruthy();
+
+  //   //   const resetInputEl = findEl(fixture, 'date').nativeElement;
+  //   //   //console.log(resetInputEl);
+  //   //   resetInputEl.value = '2022-08-27';
+
+  //   //   resetInputEl.dispatchEvent(new Event('change'));
+
+
+  //   //   fixture.detectChanges();
+
+  //   //   tick(1000);
+
+  //   //   console.log(resetInputEl.value);
+  //   //   // console.log(component.date);
+  //   //   // console.log(component.dateForm);
+
+  //   //   //expectText(fixture, 'form-invalid-date-error-message', 'Choose a time using the control.');
+  //   //   console.log(component.date)
+  //   //   expect(component.updateFormValue).toHaveBeenCalled();
+  //   //   // expectText(fixture, 'form-invalid-time-error-message', 'Choose a time using the control.');
+  //   // }));
+
+  //   it('should show error message', fakeAsync(async () => {
+
+  //     // spyOn(component, 'updateFormValue');
+
+  //     await setup(new Date());
+
+  //     // expect(component).toBeTruthy();
+
+  //     const resetInputEl = findEl(fixture, 'date').nativeElement;
+  //     console.log(resetInputEl);
+  //     resetInputEl.value = '2022-08-27';
+
+  //     resetInputEl.dispatchEvent(new Event('input'));
+
+  //     click(fixture, "date");
+
+  //     resetInputEl.value = '2022-08-27';
+  //     fixture.detectChanges();
+  //     resetInputEl.value = '2022-08-27';
+  //     tick(1000);
+
+  //     console.log(resetInputEl);
+  //     // console.log(component.date);
+  //     // console.log(component.dateForm);
+
+  //     //expectText(fixture, 'form-invalid-date-error-message', 'Choose a time using the control.');
+  //     console.log(component.time)
+  //     expect(component.updateFormValue).toHaveBeenCalled();
+  //     // expectText(fixture, 'form-invalid-time-error-message', 'Choose a time using the control.');
+  //   }));
+  // });
+
+});
--- src/app/_observation/select-date-time/select-date-time.component.ts
+++ src/app/_observation/select-date-time/select-date-time.component.ts
@@ -1,10 +1,6 @@
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbDate, NgbDateStruct, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
-
-import { Directive } from '@angular/core';
-import { NG_VALIDATORS, Validator } from '@angular/forms';
-
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-select-date-time',
@@ -22,10 +18,10 @@ export class SelectDateTimeComponent implements OnInit {
   public spinners = true;
 
   ngOnInit(): void {
-    this._setInitialValue();
+    this._setInitialValues();
   }
 
-  private _setInitialValue(): void {
+  private _setInitialValues(): void {
     const formValue = new Date(this.dateForm.value.observationDateTime);
 
     this.date = new NgbDate(formValue.getFullYear(), formValue.getMonth() + 1, formValue.getDate());
@@ -51,8 +47,13 @@ export class SelectDateTimeComponent implements OnInit {
   }
 
   private _updateFormValue(): void {
-    //guards
-    // if null
+
+    if (this.time == null || this.date == null)
+    {
+      this.dateForm.setErrors({ 'invalid': true });
+      // this.dateForm.controls['observationDateTime'].setValue(null);
+    }
+
     const newDateTime = new Date(this.date.year, this.date.month - 1, this.date.day, this.time.hour, this.time.minute);
 
     this.dateForm.setValue(
@@ -60,52 +61,5 @@ export class SelectDateTimeComponent implements OnInit {
         observationDateTime: newDateTime
       }
     );
-  }
-
-  @Input('appForbiddenName') forbiddenName = '';
-
-  validate(control: AbstractControl): ValidationErrors | null {
-    const value = control.value;
-
-    alert(control.value);
-
-    if (!value) {
-      return null;
-    }
-
-    if (value.hour < 12) {
-      return { tooEarly: true };
-    }
-    if (value.hour > 13) {
-      return { tooLate: true };
-    }
-
-    return null;
-  }
-}
-
-
-
-
-/** A hero's name can't match the given regular expression */
-export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    alert('');
-    const forbidden = nameRe.test(control.value);
-    return forbidden ? { forbiddenName: { value: control.value } } : null;
-  };
-}
-
-@Directive({
-  selector: '[appForbiddenName]',
-  providers: [{ provide: NG_VALIDATORS, useExisting: ForbiddenValidatorDirective, multi: true }]
-})
-export class ForbiddenValidatorDirective implements Validator {
-  @Input('appForbiddenName') forbiddenName = '';
-
-  validate(control: AbstractControl): ValidationErrors | null {
-    alert('');
-    return this.forbiddenName ? forbiddenNameValidator(new RegExp(this.forbiddenName, 'i'))(control)
-      : null;
   }
 }

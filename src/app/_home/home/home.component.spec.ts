81302032578cbd0223927ca96990645ec3dc9110
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-01-20 17:52:53 +0000

    Update Home comp

--- src/app/_home/home/home.component.spec.ts
+++ src/app/_home/home/home.component.spec.ts
@@ -4,15 +4,24 @@ import { provideRouter, Router, Routes } from '@angular/router';
 import { AuthenticationService } from 'src/app/_auth/authentication.service';
 import { of } from 'rxjs';
 import { ObservationFeedComponent } from 'src/app/_observation-feed/observation-feed/observation-feed.component';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { LoginComponent } from 'src/app/_auth/login/login.component';
+import { AccountRegistrationComponent } from 'src/app/_account/account-registration/account-registration.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
+  let loader: HarnessLoader;
   let fixture: ComponentFixture<HomeComponent>;
   let fakeService: jasmine.SpyObj<AuthenticationService>;
   let router: Router;
 
   const routes: Routes = [
-    { path: 'feed-p/public', component: ObservationFeedComponent }
+    { path: 'feed-p/public', component: ObservationFeedComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'account/registration', component: AccountRegistrationComponent },
   ];
 
   const setup = async (
@@ -33,7 +42,7 @@ describe('HomeComponent', () => {
     );
 
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HomeComponent, NoopAnimationsModule],
       providers: [
         provideRouter(routes),
         { provide: AuthenticationService, useValue: fakeService }
@@ -45,6 +54,8 @@ describe('HomeComponent', () => {
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
+
     fixture.detectChanges();
   };
 
@@ -76,4 +87,153 @@ describe('HomeComponent', () => {
       expect(router.navigate).toHaveBeenCalledWith(['/feed-p/public']);
     }));
   });
+
+  describe('', () => {
+
+    it('should render the Login button', async () => {
+      await setup({ isLoggedIn: false });
+
+      const btn = await loader.getHarness(MatButtonHarness.with({ selector: '#btn-login' }));
+      expect(await btn.isDisabled()).toBe(false);
+      expect(await btn.getText()).toContain('I already have an account');
+    });
+
+    it('should render the Login button', async () => {
+      await setup({ isLoggedIn: false });
+
+      const btn = await loader.getHarness(MatButtonHarness.with({ selector: '#btn-login-two' }));
+      expect(await btn.isDisabled()).toBe(false);
+      expect(await btn.getText()).toContain('I already have an account');
+    });
+
+    it('should render the Register button', async () => {
+      await setup({ isLoggedIn: false });
+
+      const btn = await loader.getHarness(MatButtonHarness.with({ selector: '#btn-register' }));
+      expect(await btn.isDisabled()).toBe(false);
+      expect(await btn.getText()).toContain('Get started');
+    });
+
+    it('should render the Login button', async () => {
+      await setup({ isLoggedIn: false });
+
+      const btn = await loader.getHarness(MatButtonHarness.with({ selector: '#btn-register-two' }));
+      expect(await btn.isDisabled()).toBe(false);
+      expect(await btn.getText()).toContain('Get started');
+    });
+
+
+    // it('should open Login page', async () => {
+    //   await setup({ isLoggedIn: false });
+
+    //   // const btn = await loader.getHarness(MatButtonHarness.with({ selector: '#btn-register' }));
+    //   // await btn.click();
+
+    //   fixture.debugElement.query(By.css('.button-poiu')).triggerEventHandler('click', null);
+    //   // expect(routerStub.navigate).toHaveBeenCalledWith(['/home']
+    //   expect(router.navigate).toHaveBeenCalledWith(['/home']);
+    // });
+  });
 });
+
+
+// https://dev.to/this-is-angular/testing-angular-routing-components-with-routertestingharness-providelocationmocks-and-providerouter-oi8
+
+
+// import { Location } from '@angular/common';
+// import { provideLocationMocks } from '@angular/common/testing';
+// import { Location } from '@angular/common';
+// import { provideLocationMocks } from '@angular/common/testing';
+// import { Component } from '@angular/core';
+// import {
+//   fakeAsync,
+//   TestBed,
+//   tick,
+// } from '@angular/core/testing';
+// import { By } from '@angular/platform-browser';
+// import { provideRouter } from '@angular/router';
+// import { RouterTestingHarness } from '@angular/router/testing';
+// import { asapScheduler, of } from 'rxjs';
+// import { observeOn } from 'rxjs/operators';
+
+// import { HeroService } from '../hero.service';
+// import { HEROES } from '../mock-heroes';
+// import { DashboardComponent } from './dashboard.component';
+
+// async function setup() {
+//   const fakeService = jasmine.createSpyObj<AuthenticationService>(
+//     'AuthenticationService',
+//     {
+//       isLoggedIn: undefined,
+//       checkAuthStatus: undefined,
+//       logout: undefined,
+//     },
+//     {
+//       isAuthorisedObservable: of(false),
+//       getAuthUser: undefined
+//     }
+//   );
+
+//   TestBed.configureTestingModule({
+//     providers: [
+//       provideRouter([
+//         { path: 'feed-p/public', component: ObservationFeedComponent },
+//         { path: 'login', component: LoginComponent },
+//         { path: 'account/registration', component: AccountRegistrationComponent },
+//       ]),
+//       provideLocationMocks(),
+//       { provide: AuthenticationService, useValue: fakeService },
+//     ],
+//   });
+
+//   const harness = await RouterTestingHarness.create(); // [1]
+//   const location = TestBed.inject(Location);
+
+//   return {
+//     advance() {
+//       tick();
+//       harness.detectChanges();
+//     },
+//     clickTopHero() {
+//       const firstHeroLink = harness.routeDebugElement?.query(
+//         By.css('.button-poiu')
+//       );
+
+//       firstHeroLink?.triggerEventHandler('click', {
+//         button: leftMouseButton,
+//       });
+//     },
+//     harness,
+//     location,
+//   };
+// }
+
+// @Component({
+//   standalone: true,
+//   template: '',
+// })
+// class TestHeroDetailComponent { }
+
+// const leftMouseButton = 0;
+
+// describe('DashboardComponent (integrated)', () => {
+//   it('navigates to the detail view when a hero link is clicked', fakeAsync(async () => {
+//     const { advance, clickTopHero, harness, location } =
+//       await setup();
+//     const component /* [2] */ = await harness.navigateByUrl(
+//       '/account/registration',
+//       AccountRegistrationComponent // [3]
+//     );
+//     // const [topHero] = component.heroes;
+
+//     clickTopHero();
+//     advance();
+
+//     const expectedPath = '/account/registration';
+//     expect(location.path())
+//       .withContext(
+//         'must navigate to the detail view for the top hero'
+//       )
+//       .toBe(expectedPath);
+//   }));
+// });

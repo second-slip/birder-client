786beaab4c4f18f1b0a3584dc6bdc3313ef1760f
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-01-21 18:58:48 +0000

    More work on new About pages

--- src/app/_about/about-general/about-general.component.ts
+++ src/app/_about/about-general/about-general.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-about-general',
   standalone: true,
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './about-general.component.html',
   styleUrl: './about-general.component.scss'
 })
-export class AboutGeneralComponent {
-
-}
+export class AboutGeneralComponent { }
--- src/app/_about/about/about.component.spec.ts
+++ src/app/_about/about/about.component.spec.ts
@@ -1,30 +1,79 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AboutComponent } from './about.component';
 import { provideRouter } from '@angular/router';
 import { blankRoutesArray } from 'src/app/testing/route-tests-helpers';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatTabGroupHarness } from '@angular/material/tabs/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { By } from '@angular/platform-browser';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutComponent],
+      imports: [AboutComponent, NoopAnimationsModule],
       providers: [
         provideRouter(blankRoutesArray),
-        provideAnimations()
       ],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('only renders the About General component in the default About tab', () => {
+    const { debugElement } = fixture;
+    expect(debugElement.query(By.css('app-about-general'))).toBeTruthy();
+    expect(debugElement.query(By.css('app-features'))).toBeFalsy();
+  });
+
+  it('should load harness for tab-group', async () => {
+    const tabGroups = await loader.getAllHarnesses(MatTabGroupHarness);
+    expect(tabGroups.length).toBe(1);
+  });
+
+  it('should load harness for tab-group with selected tab label', async () => {
+    const tabGroups = await loader.getAllHarnesses(
+      MatTabGroupHarness.with({
+        selector: '#about-tab-group',
+      }),
+    );
+    expect(tabGroups.length).toBe(1);
+  });
+
+  describe('test tab group functuionality', () => {
+
+    it('should be able to get tabs of tab-group', async () => {
+      const tabGroup = await loader.getHarness(MatTabGroupHarness);
+      const tabs = await tabGroup.getTabs();
+      expect(tabs.length).toBe(3);
+    });
+
+    it('should be able to select and switch tabs from tab-group and render the correct content', async () => {
+      const { debugElement } = fixture;
+
+      const tabGroup = await loader.getHarness(MatTabGroupHarness);
+      expect(await (await tabGroup.getSelectedTab()).getLabel()).toBe('sentiment_very_satisfied About');
+
+      expect(debugElement.query(By.css('app-about-general'))).toBeTruthy();
+      expect(debugElement.query(By.css('app-features'))).toBeFalsy();
+
+      await tabGroup.selectTab({ label: 'list_alt Features' });
+      expect(await (await tabGroup.getSelectedTab()).getLabel()).toBe('list_alt Features');
+
+      expect(debugElement.query(By.css('app-about-general'))).toBeFalsy();
+      expect(debugElement.query(By.css('app-features'))).toBeTruthy();
+    });
   });
 });

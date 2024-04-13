b2af7a612a995f0423432e56ea5756b372ef4d9f
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-04-13 20:01:54 +0100

    more c/f refractoring

--- src/app/_observation-feed/observation-feed-item/observation-feed-item.component.ts
+++ src/app/_observation-feed/observation-feed-item/observation-feed-item.component.ts
@@ -3,19 +3,18 @@ import { AuthenticationService } from 'src/app/_auth/authentication.service';
 import { IObservationFeed } from '../i-observation-feed.dto';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-import { NgIf, AsyncPipe, DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
-    selector: 'app-observation-feed-item',
-    templateUrl: './observation-feed-item.component.html',
-    styleUrls: ['./observation-feed-item.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    standalone: true,
-    imports: [NgIf, RouterLink, MatIconModule, AsyncPipe, DatePipe]
+  selector: 'app-observation-feed-item',
+  templateUrl: './observation-feed-item.component.html',
+  styleUrls: ['./observation-feed-item.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: [RouterLink, MatIconModule, AsyncPipe, DatePipe]
 })
 export class ObservationFeedItemComponent {
   @Input() observation: IObservationFeed;
 
   constructor(readonly _authService: AuthenticationService) { }
-
 }
--- src/app/_tweet/tweet-day/tweet-day.component.ts
+++ src/app/_tweet/tweet-day/tweet-day.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TweetDayService } from './tweet-day.service';
 import { LoadingComponent } from '../../_loading/loading/loading.component';
 import { RouterLink } from '@angular/router';
-import { NgIf, AsyncPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'app-tweet-day',
@@ -10,7 +10,7 @@ import { NgIf, AsyncPipe } from '@angular/common';
     styleUrls: ['./tweet-day.component.scss'],
     encapsulation: ViewEncapsulation.None,
     standalone: true,
-    imports: [NgIf, RouterLink, LoadingComponent, AsyncPipe]
+    imports: [RouterLink, LoadingComponent, AsyncPipe]
 })
 export class TweetDayComponent implements OnInit {
 

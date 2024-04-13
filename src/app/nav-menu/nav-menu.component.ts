927731210d9acf113b735b2fca87da45532c1653
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-04-13 13:11:34 +0100

    refactor to c/f style

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AuthenticationService } from '../_auth/authentication.service';
 import { MatIconModule } from '@angular/material/icon';
-import { NgIf, AsyncPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -10,10 +10,9 @@ import { RouterLink } from '@angular/router';
     styleUrls: ['./nav-menu.component.scss'],
     encapsulation: ViewEncapsulation.None,
     standalone: true,
-    imports: [RouterLink, NgIf, MatIconModule, AsyncPipe]
+    imports: [RouterLink, MatIconModule, AsyncPipe]
 })
 export class NavMenuComponent implements OnInit {
-  isNavbarCollapsed = true;
 
   constructor(readonly _service: AuthenticationService) { }
 
@@ -25,4 +24,3 @@ export class NavMenuComponent implements OnInit {
     this._service.checkAuthStatus();
   }
 }
-

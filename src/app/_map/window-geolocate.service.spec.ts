bf317b7a82fb84e5069dd0fe0bafd77e8a4cf53d
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-05-10 23:49:24 +0100

    tests for new service

--- src/app/_map/window-geolocate.service.spec.ts
+++ src/app/_map/window-geolocate.service.spec.ts
@@ -1,172 +1,96 @@
 import { TestBed } from '@angular/core/testing';
-
 import { WindowGeolocateService } from './window-geolocate.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('WindowGeolocateService', () => {
   let service: WindowGeolocateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA]
+    });
     service = TestBed.inject(WindowGeolocateService);
   });
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
+  describe('Get Current Location', () => {
+
+    it('calls window.navigator.geolocation.getCurrentPosition when method is called', () => {
+
+      const expected = <GeolocationPosition>{
+        coords: { latitude: 0, longitude: 0 },
+        timestamp: 5467
+      }
+
+      spyOn(navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
+        // const position = { coords: { latitude: 0, longitude: 0 } };
+        args[0](expected);
+      });
+
+      let actual: any | undefined;
+
+      service.getCurrentPosition().subscribe((otherResult) => {
+        // console.log(otherResult);
+        actual = otherResult;
+      });
+
+      // Assert
+      expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalledTimes(1);
+    });
+
+    it('getCurrentPosition NEXT returns GeolocationPosition object', () => {
+
+      const expected = <GeolocationPosition>{
+        coords: { latitude: 0, longitude: 0 },
+        timestamp: 5467
+      }
+
+      spyOn(navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
+        // const position = { coords: { latitude: 0, longitude: 0 } };
+        args[0](expected);
+      });
+
+      let actual: any | undefined;
+
+      service.getCurrentPosition().subscribe((otherResult) => {
+        actual = otherResult;
+      });
+
+      // Assert
+      expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalledTimes(1);
+      expect(actual).toBe(expected);
+    });
+
+    it('getCurrentPosition ERROR returns GeolocationPosition object', () => {
+
+      const expected = <GeolocationPositionError>{
+        code: 1,
+        message: 'GeoLocation Error'
+      };
+
+      spyOn(navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
+        args[1](expected);
+      });
+
+      let actual: any | undefined;
+
+      service.getCurrentPosition()
+        .subscribe({
+          next: (position) => { },
+          error: (error) => {
+            actual = error
+          },
+        });
+
+      // Assert
+      expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalledTimes(1);
+      expect(window.navigator.geolocation.getCurrentPosition).toThrowError()
+      expect(actual).toEqual(expected);
+    });
+  });
 });
-
-// describe('Get Current Location', () => {
-
-//   it('calls ', () => {
-//     spyOn(window.navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
-//       const position = { coords: { latitude: 0, longitude: 0 } };
-//       args[0](position);
-//     });
-
-//     component.latitude = testLatitude;
-//     component.longitude = testLongitude;
-//     fixture.detectChanges();
-
-//     // button click...
-//     fixture.debugElement.query(By.css('.btn-get-location')).triggerEventHandler('click', null);
-
-//     expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
-
-//     expect(fakeService.reverseGeocode).toHaveBeenCalled();
-//   });
-
-//   it('clears geoError property if it is truthy', () => {
-//     spyOn(window.navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
-//       const position = { coords: { latitude: 0, longitude: 0 } };
-//       args[0](position);
-//     });
-
-//     component.latitude = testLatitude;
-//     component.longitude = testLongitude;
-//     component.geoError = 'truthy';
-//     fixture.detectChanges();
-
-//     // button click...
-//     fixture.debugElement.query(By.css('.btn-get-location')).triggerEventHandler('click', null);
-
-//     fixture.detectChanges();
-
-//     expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
-//     expect(component.geoError).toBeFalsy();
-//   });
-
-//   it('y error', () => {
-//     spyOn(window.navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
-//       const error = {
-//         code: 1,
-//         message: 'GeoLocation Error',
-//       };
-//       args[1](error);
-//     });
-
-//     component.latitude = testLatitude;
-//     component.longitude = testLongitude;
-//     fixture.detectChanges();
-
-//     // button click...
-//     fixture.debugElement.query(By.css('.btn-get-location')).triggerEventHandler('click', null);
-
-//     expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
-//     expect(window.navigator.geolocation.getCurrentPosition).toThrowError();
-
-//     fixture.detectChanges();
-
-//     expectText(fixture, 'geo-error', 'User denied the request for Geolocation...');
-//   });
-
-//   it('y error', () => {
-//     spyOn(window.navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
-//       const error = {
-//         code: 2,
-//         message: 'GeoLocation Error',
-//       };
-//       args[1](error);
-//     });
-
-//     component.latitude = testLatitude;
-//     component.longitude = testLongitude;
-
-//     fixture.detectChanges();
-//     // button click...
-//     fixture.debugElement.query(By.css('.btn-get-location')).triggerEventHandler('click', null);
-
-//     expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
-//     expect(window.navigator.geolocation.getCurrentPosition).toThrowError();
-
-//     fixture.detectChanges();
-
-//     expectText(fixture, 'geo-error', 'Location information is unavailable...');
-//   });
-
-//   it('y error', () => {
-//     spyOn(window.navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
-//       const error = {
-//         code: 3,
-//         message: 'GeoLocation Error',
-//       };
-//       args[1](error);
-//     });
-
-//     component.latitude = testLatitude;
-//     component.longitude = testLongitude;
-//     fixture.detectChanges();
-
-//     // button click...
-//     fixture.debugElement.query(By.css('.btn-get-location')).triggerEventHandler('click', null);
-
-//     expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
-//     expect(window.navigator.geolocation.getCurrentPosition).toThrowError();
-
-//     fixture.detectChanges();
-
-//     expectText(fixture, 'geo-error', 'The request to get user location timed out...');
-//   });
-
-//   it('y error', () => {
-//     spyOn(window.navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
-//       const error = {};
-//       args[1](error);
-//     });
-
-//     component.latitude = testLatitude;
-//     component.longitude = testLongitude;
-//     fixture.detectChanges();
-
-//     // button click...
-//     fixture.debugElement.query(By.css('.btn-get-location')).triggerEventHandler('click', null);
-
-//     expect(window.navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
-//     expect(window.navigator.geolocation.getCurrentPosition).toThrowError();
-
-//     fixture.detectChanges();
-
-//     expectText(fixture, 'geo-error', 'An error occurred with Geolocation...');
-//   });
-
-//   it('clears geoError property if it is truthy', () => {
-//     spyOn(window.navigator.geolocation, 'getCurrentPosition').and.callFake((...args: any[]) => {
-//       const error = {};
-//       args[1](error);
-//     });
-
-//     component.latitude = testLatitude;
-//     component.longitude = testLongitude;
-//     component.geoError = 'truthy';
-//     fixture.detectChanges();
-
-//     // button click...
-//     fixture.debugElement.query(By.css('.btn-get-location')).triggerEventHandler('click', null);
-
-
-//     fixture.detectChanges();
-
-//     expectText(fixture, 'geo-error', 'An error occurred with Geolocation...');
-//   });
-
-
-// });

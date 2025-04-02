96c2ef0755665de0f382811d40fbe2b7b39b19a1
Andrew Cross <andrew-stuart-cross@outlook.com>
2025-04-02 12:36:14 +0100

    subscribe to new network changed event publisher

--- src/app/_network/network-suggestion/network-suggestion.component.ts
+++ src/app/_network/network-suggestion/network-suggestion.component.ts
@@ -3,18 +3,28 @@ import { NetworkSuggestionService } from './network-suggestion.service';
 import { LoadingComponent } from '../../_loading/loading/loading.component';
 import { NetworkUserComponent } from '../network-user/network-user.component';
 import { AsyncPipe } from '@angular/common';
+import { AnnounceChangesService } from 'src/app/_sharedServices/announce-changes.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'app-network-suggestion',
-    templateUrl: './network-suggestion.component.html',
-    styleUrls: ['./network-suggestion.component.scss'],
-    providers: [NetworkSuggestionService],
-    encapsulation: ViewEncapsulation.None,
-    imports: [NetworkUserComponent, LoadingComponent, AsyncPipe]
+  selector: 'app-network-suggestion',
+  templateUrl: './network-suggestion.component.html',
+  styleUrls: ['./network-suggestion.component.scss'],
+  providers: [NetworkSuggestionService],
+  encapsulation: ViewEncapsulation.None,
+  imports: [NetworkUserComponent, LoadingComponent, AsyncPipe],
 })
 export class NetworkSuggestionComponent implements OnInit {
-
-  constructor(readonly _service: NetworkSuggestionService) { }
+  constructor(
+    protected readonly _service: NetworkSuggestionService,
+    private _announcement: AnnounceChangesService
+  ) {
+    this._announcement.networkChanged$
+      .pipe(takeUntilDestroyed())
+      .subscribe((msg) => {
+        this._service.getData();
+      });
+  }
 
   ngOnInit(): void {
     this._getData();
--- src/app/_network/network-summary/network-summary.component.ts
+++ src/app/_network/network-summary/network-summary.component.ts
@@ -4,19 +4,31 @@ import { NetworkSummaryService } from './network-summary.service';
 import { LoadingComponent } from '../../_loading/loading/loading.component';
 import { RouterLink } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
+import { AnnounceChangesService } from 'src/app/_sharedServices/announce-changes.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'app-network-summary',
-    templateUrl: './network-summary.component.html',
-    styleUrls: ['./network-summary.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    imports: [RouterLink, LoadingComponent, AsyncPipe]
+  selector: 'app-network-summary',
+  templateUrl: './network-summary.component.html',
+  styleUrls: ['./network-summary.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  imports: [RouterLink, LoadingComponent, AsyncPipe],
 })
 export class NetworkSummaryComponent implements OnInit {
   @Input()
   public showTitle: boolean;
 
-  constructor(readonly _service: NetworkSummaryService, readonly _authService: AuthenticationService) { }
+  constructor(
+    protected readonly _service: NetworkSummaryService,
+    protected readonly _authService: AuthenticationService,
+    private _announcement: AnnounceChangesService
+  ) {
+    this._announcement.networkChanged$
+      .pipe(takeUntilDestroyed())
+      .subscribe((msg) => {
+        this._service.getData();
+      });
+  }
 
   ngOnInit(): void {
     this._getData();
